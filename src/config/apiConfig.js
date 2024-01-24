8ead8b2d288528addd8f9a5c3f031169eee787d8
swarajkaushal <swarajkaushal802@gmail.com>
2024-01-24 14:05:05 +0530

    first commit

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-export const API_BASE_URL ="http://localhost:5454"
+// export const API_BASE_URL ="http://localhost:5454"
+export const API_BASE_URL ="e-commerce-shop-backend-production.up.railway.app"
 const jwt = localStorage.getItem('jwt');
 
  export const api = axios.create({
